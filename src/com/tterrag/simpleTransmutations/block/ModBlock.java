b288326475a237cf9eb975df716c7a16a4b93e36
tterrag1098 <tterrag1098@gmail.com>
2013-09-22 20:14:14 -0400

    Removing experimental changes to put them in experimental branch

--- src/com/tterrag/simpleTransmutations/block/ModBlock.java
+++ src/com/tterrag/simpleTransmutations/block/ModBlock.java
@@ -1,20 +1,14 @@
 package com.tterrag.simpleTransmutations.block;
 
 import net.minecraft.block.Block;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class ModBlock {
 
 	public static Block chainSmelter;
 	
 	public static void init() {
-		chainSmelter = new BlockChainSmelter(BlockInfo.CHAIN_SMELTER_ID, false);
-		GameRegistry.registerBlock(chainSmelter, BlockInfo.CHAIN_SMELTER_KEY);
 	}
 
 	public static void addNames() {
-		LanguageRegistry.addName(chainSmelter,
-				BlockInfo.CHAIN_SMELTER_LOC_NAME);
 	}
 }
