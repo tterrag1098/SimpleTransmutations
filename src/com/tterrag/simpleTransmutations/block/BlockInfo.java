a93c2f117a988f2d53c06343cc1aa4deaf11e46c
tterrag1098 <tterrag1098@gmail.com>
2013-09-24 19:40:07 -0400

    Minor edits

--- src/com/tterrag/simpleTransmutations/block/BlockInfo.java
+++ src/com/tterrag/simpleTransmutations/block/BlockInfo.java
@@ -6,10 +6,17 @@ public class BlockInfo {
 		public static final String TEXTURE_LOC = "example";
 
 		//Wand of the Rocket info
-		public static int CHAIN_SMELTER_ID = 4095;
+		public static int CHAIN_SMELTER_ID;
 		public static final String CHAIN_SMELTER_KEY = "cSmelter";
 		public static final int CHAIN_SMELTER_DEFAULT = 3000;
 		public static final String CHAIN_SMELTER_UNLOC_NAME = "chainSmelter";
 		public static final String CHAIN_SMELTER_LOC_NAME = "Chain Smelter";
 		public static final String CHAIN_SMELTER_ICON = "chainSmelter";
+		
+		public static int INVISIBLE_REDSTONE_ID = 3001;
+		public static final String INVISIBLE_REDSTONE_KEY = "invisRedstone";
+		public static final int INVISIBLE_REDSTONE_DEFAULT = 3001;
+		public static final String INVISIBLE_REDSTONE_UNLOC_NAME = "invisibleRedstone";
+		public static final String INVISIBLE_REDSTONE_LOC_NAME = "DON'T TOUCH";
+		public static final String INVISIBLE_REDSTONE_ICON = "invisibleRedstone";
 }
--- src/com/tterrag/simpleTransmutations/block/ModBlock.java
+++ src/com/tterrag/simpleTransmutations/block/ModBlock.java
@@ -1,11 +1,8 @@
 package com.tterrag.simpleTransmutations.block;
 
-import net.minecraft.block.Block;
 
 public class ModBlock {
 
-	public static Block chainSmelter;
-	
 	public static void init() {
 	}
 
