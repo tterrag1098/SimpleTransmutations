e676ff82201972e62239f8c9caf0a2e134103f2b
tterrag1098 <tterrag1098@gmail.com>
2013-10-22 17:44:24 -0400

    Beginning to implement essence container

--- src/com/tterrag/simpleTransmutations/item/ItemEssenceContainer.java
+++ src/com/tterrag/simpleTransmutations/item/ItemEssenceContainer.java
@@ -4,7 +4,13 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemEssenceContainer extends Item
 {
@@ -22,5 +28,20 @@ public class ItemEssenceContainer extends Item
 	{
 		itemIcon = register.registerIcon(ItemInfo.TEXTURE_LOC + ":"
 				+ ItemInfo.ESSENCE_CONTAINER_ICON);
+	}
+	
+	@Override
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player,
+			Entity entity)
+	{
+		EntityLivingBase entityLiving;
+		if (!player.worldObj.isRemote && entity instanceof EntityLivingBase && ((EntityLivingBase) entity).getHealth() > 0)
+		{
+			entityLiving = (EntityLivingBase) entity;
+			entityLiving.attackEntityFrom(null, 2);
+			System.out.println(entityLiving.getEntityName());
+			System.out.println(entityLiving.getHealth());
+		}		
+		return super.onLeftClickEntity(stack, player, entity);
 	}
 }
